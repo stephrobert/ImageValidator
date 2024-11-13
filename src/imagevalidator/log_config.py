5943ef1dcead2486cc3caef6edea62a243eaa6dc
Stéphane ROBERT <robert.stephane.28@gmail.com>
2024-11-13 16:13:08 +0000

    fix: Utilisation de la configuration fournie dans le fichier

--- src/imagevalidator/log_config.py
+++ src/imagevalidator/log_config.py
@@ -1,16 +1,23 @@
-# src/log_config.py
 import logging
+import yaml
 
 
-def setup_logging(logging_config):
+def setup_logging(config_path="logging_config.yaml"):
+    # Charge le fichier YAML comme un dictionnaire
+    with open(config_path, "r") as file:
+        logging_config = yaml.safe_load(file)
+
+    # Vérifie si le chargement a produit un dictionnaire
+    if not isinstance(logging_config, dict):
+        raise ValueError(
+            "Le fichier de configuration de logging doit être un dictionnaire."
+        )
+
+    # Utilisez le dictionnaire pour configurer le logging
     log_format = logging_config.get(
-        "format", "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        "format", "%(asctime)s - %(levelname)s - %(message)s"
     )
-    log_level = logging_config.get("level", "INFO").upper()
+    log_level = logging_config.get("level", "INFO")
 
-    logging.basicConfig(
-        format=log_format,
-        level=getattr(logging, log_level, logging.INFO),
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-    logging.info("Logging configuré avec succès")
+    # Configure le logging
+    logging.basicConfig(level=log_level, format=log_format)
